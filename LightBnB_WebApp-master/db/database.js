a12efd3a24f08be66517990774fdc7fd81038764
Angela Zhou <a03zhou@gmail.com>
2023-09-09 01:15:23 -0400

    revise getUserWithEmail, getUserWithId, addUser functions with SQL queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -10,7 +10,7 @@ const pool = new Pool({
   host: 'localhost',
   database: 'lightbnb'
 });
-pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {})
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {console.log()})
 
 /// Users
 
@@ -20,14 +20,19 @@ pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {})
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function (email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user && user.email.toLowerCase() === email.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query(`
+    SELECT * 
+    FROM users
+    WHERE users.email = $1
+    `, [email])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -36,7 +41,19 @@ const getUserWithEmail = function (email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function (id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(`
+    SELECT * 
+    FROM users
+    WHERE users.id = $1
+    `, [id])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -45,10 +62,19 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(`
+      INSERT INTO users (name, email, password)
+      VALUES ($1, $2, $3)
+      RETURNING *;
+    `, [user.name, user.email, user.password])
+    .then((result) => {
+      console.log(result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Reservations
@@ -72,7 +98,11 @@ const getAllReservations = function (guest_id, limit = 10) {
  */
 const getAllProperties = (options, limit = 10) => {
   return pool
-    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .query(`
+    SELECT * 
+    FROM properties 
+    LIMIT $1
+    `, [limit])
     .then((result) => {
       console.log(result.rows);
       return result.rows;
