857bc107089098358b90422dd1b53789b13ef771
Angela Zhou <a03zhou@gmail.com>
2023-09-08 21:40:14 -0400

    lightbnb boilerplate code - revise getAllProperties

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -1,6 +1,17 @@
 const properties = require("./json/properties.json");
 const users = require("./json/users.json");
 
+// Connect to PostgreSQL
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'lightbnb'
+});
+pool.query(`SELECT title FROM properties LIMIT 10;`).then(response => {})
+
 /// Users
 
 /**
@@ -59,12 +70,16 @@ const getAllReservations = function (guest_id, limit = 10) {
  * @param {*} limit The number of results to return.
  * @return {Promise<[{}]>}  A promise to the properties.
  */
-const getAllProperties = function (options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+const getAllProperties = (options, limit = 10) => {
+  return pool
+    .query(`SELECT * FROM properties LIMIT $1`, [limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
